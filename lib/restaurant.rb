848fb0d0ec55dda7f6de344046e8c339ba87585e
Grayson Cunningham <gecun289@gmail.com>
2020-10-07 13:24:40 -0600

    complete iteration 2

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -7,4 +7,12 @@ class Restaurant
     @dishes = []
   end
 
+  def closing_time(hours)
+    closing_time = opening_time.to_i + hours
+    closing_time = closing_time.to_s.concat(":00")
+  end
+
+  def add_dish(food)
+    @dishes << food
+  end
 end
